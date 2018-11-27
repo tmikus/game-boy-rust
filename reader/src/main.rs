f05b41dc1b7942774d25c042852db19747f52536
Tomasz Mikus <mikus.tomasz@gmail.com>
2018-11-27 19:33:07 +0000

    Might be the finished implementation.

--- reader/src/main.rs
+++ reader/src/main.rs
@@ -4,6 +4,9 @@ use {
   rppal::gpio::{ Gpio, Level, Mode },
   std::{
     collections::LinkedList,
+    fs,
+    thread::sleep,
+    time::Duration,
   },
 };
 
@@ -58,7 +61,7 @@ impl<'a> Shifter<'a> {
     self.invert = !self.invert;
   }
   
-  pub fn apply(&mut self) {
+  pub fn write(&mut self) {
     self.gpio.write(self.latch_pin, Level::Low);
     for register in self.shift_registers.iter() {
       for number in 0..register.pins {
@@ -115,6 +118,50 @@ const DATA_6_ID: u8 = 35;
 const DATA_7_ID: u8 = 37;
 
 fn main() {
-  let gpio = Gpio::new().unwrap();
+  println!("Start reading");
+  let mut gpio = Gpio::new().unwrap();
+  // Setup output pins
+  gpio.set_mode(LATCH_PIN_ID, Mode::Output);
+  gpio.set_mode(CLOCK_PIN_ID, Mode::Output);
+  gpio.set_mode(DATA_PIN_ID, Mode::Output);
+  gpio.set_mode(READ_PIN_ID, Mode::Output);
+  gpio.set_mode(WRITE_PIN_ID, Mode::Output);
+  // Setup input pins
+  let data_pins = [
+    DATA_0_ID,
+    DATA_1_ID,
+    DATA_2_ID,
+    DATA_3_ID,
+    DATA_4_ID,
+    DATA_5_ID,
+    DATA_6_ID,
+    DATA_7_ID,
+  ];
+  for pin in data_pins.iter() {
+    gpio.set_mode(pin.clone(), Mode::Input);
+  }
+  // Initialise shifter
   let mut shifter = Shifter::new(&gpio, DATA_PIN_ID, LATCH_PIN_ID, CLOCK_PIN_ID);
+  shifter.add(8);
+  shifter.add(8);
+  gpio.write(READ_PIN_ID, Level::Low);
+  gpio.write(WRITE_PIN_ID, Level::High);
+
+  // Read data
+  let mut data: Vec<u8> = Vec::new();
+  for addr in 0..= 0x3FFF {
+    shifter.set(0, addr >> 8);
+    shifter.set(1, addr & 0xFF);
+    shifter.write();
+    sleep(Duration::from_micros(50));
+    let mut value = 0u8;
+    for (bit, pin) in data_pins.iter().enumerate() {
+      if gpio.read(pin.clone()).unwrap() == Level::High {
+        value |= 1 << bit;
+      }
+    }
+    data.push(value);
+  }
+  fs::write("rom.dat", data);
+  println!("Done");
 }
