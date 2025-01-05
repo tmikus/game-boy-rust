f1b315be3313b8c0738ffe21ba93e5f1bebfd8bc
Tomasz Mikus <mikus.tomasz@gmail.com>
2025-01-05 15:16:57 +0000

    Update the pins to match the new board design.

--- reader/src/main.rs
+++ reader/src/main.rs
@@ -4,47 +4,32 @@ use std::io::Write;
 use {
     rppal::gpio::{Gpio, Level, Mode},
     std::{
-        collections::LinkedList,
         fs,
         thread::sleep,
         time::Duration,
     },
 };
 
-// Game Boy Cartridge pinout
-// GPIO 23 = PIN 13 = RB PIN 16
-// GPIO 18 = ANALOG PIN 5 = RB PIN 12
-// GPIO 24 = PIN 11 = RB PIN 18
-// GPIO 25 = PIN 10 = RB PIN 22
-// GPIO 12 = PIN 12 = RB PIN 32
-// --------- DATA ------------
-// GPIO 17 = PIN 2 = RB PIN 11
-// GPIO 27 = PIN 3 = RB PIN 13
-// GPIO 22 = PIN 4 = RB PIN 15
-// GPIO 5  = PIN 5 = RB PIN 29
-// GPIO 6  = PIN 6 = RB PIN 31
-// GPIO 13 = PIN 7 = RB PIN 33
-// GPIO 19 = PIN 8 = RB PIN 35
-// GPIO 26 = PIN 9 = RB PIN 37
-
 const SLEEP: u64 = 50;
 
-const LATCH_PIN_ID: u8 = 22;
-const DATA_PIN_ID: u8 = 18;
-const CLOCK_PIN_ID: u8 = 32;
-const READ_PIN_ID: u8 = 12;
-const WRITE_PIN_ID: u8 = 16;
+const CS_PIN_ID: u8 = 18;
 
-const DATA_0_ID: u8 = 11;
-const DATA_1_ID: u8 = 13;
-const DATA_2_ID: u8 = 15;
-const DATA_3_ID: u8 = 29;
-const DATA_4_ID: u8 = 31;
-const DATA_5_ID: u8 = 33;
-const DATA_6_ID: u8 = 35;
-const DATA_7_ID: u8 = 37;
+const LATCH_PIN_ID: u8 = 24;
+const DATA_PIN_ID: u8 = 23;
+const CLOCK_PIN_ID: u8 = 25;
+const READ_PIN_ID: u8 = 15;
+const WRITE_PIN_ID: u8 = 14;
 
-const data_pins: [u8; 8] = [
+const DATA_0_ID: u8 = 8;
+const DATA_1_ID: u8 = 7;
+const DATA_2_ID: u8 = 11;
+const DATA_3_ID: u8 = 9;
+const DATA_4_ID: u8 = 10;
+const DATA_5_ID: u8 = 22;
+const DATA_6_ID: u8 = 21;
+const DATA_7_ID: u8 = 17;
+
+const DATA_PINS: [u8; 8] = [
     DATA_0_ID,
     DATA_1_ID,
     DATA_2_ID,
@@ -59,11 +44,11 @@ fn shift_out(gpio: &Gpio, data_pin: u8, clock_pin: u8, value: u8) {
     for number in (0..8).rev() {
         gpio.write(clock_pin, Level::Low);
         sleep(Duration::from_micros(50));
-        gpio.write(data_pin, match value >> number & 1 {
-            1 => Level::High,
-            0 => Level::Low,
-            _ => unreachable!(),
-        });
+        if (value >> number & 1) == 1 {
+            gpio.write(data_pin, Level::High);
+        } else {
+            gpio.write(data_pin, Level::Low);
+        }
         sleep(Duration::from_micros(50));
         gpio.write(clock_pin, Level::High);
         sleep(Duration::from_micros(50));
@@ -80,7 +65,7 @@ fn main() {
     gpio.set_mode(READ_PIN_ID, Mode::Output);
     gpio.set_mode(WRITE_PIN_ID, Mode::Output);
     // Setup input pins
-    for pin in data_pins.iter() {
+    for pin in DATA_PINS.iter() {
         gpio.set_mode(pin.clone(), Mode::Input);
     }
     gpio.write(READ_PIN_ID, Level::Low);
@@ -123,12 +108,11 @@ fn read_next_rom_bank(gpio: &Gpio, data: &mut Vec<u8>, start_address: u16) {
         write_address(&gpio, address);
         sleep(Duration::from_micros(SLEEP));
         let mut value = 0u8;
-        for (bit, pin) in data_pins.iter().enumerate() {
+        for (bit, pin) in DATA_PINS.iter().enumerate() {
             if gpio.read(pin.clone()).unwrap() == Level::High {
                 value |= (1 << bit) as u8;
             }
         }
-        print!("{:02X}", value);
         std::io::stdout().flush().unwrap();
         data.push(value);
     }
@@ -138,25 +122,25 @@ fn select_rom_bank(gpio: &mut Gpio, bank: u16) {
     gpio.write(READ_PIN_ID, Level::High);
     gpio.write(WRITE_PIN_ID, Level::Low);
     sleep(Duration::from_micros(5));
-    for pin in data_pins.iter() {
+    for pin in DATA_PINS.iter() {
         gpio.set_mode(pin.clone(), Mode::Output);
     }
     write_address(&gpio, 0x2100);
     sleep(Duration::from_micros(5));
-    for (index, pin) in data_pins.iter().enumerate() {
+    for (index, pin) in DATA_PINS.iter().enumerate() {
         if bank & (1 << index) != 0 {
-            gpio.write(pin.clone(), Level::High);
+            gpio.write(*pin, Level::High);
         } else {
-            gpio.write(pin.clone(), Level::Low);
+            gpio.write(*pin, Level::Low);
         }
     }
     sleep(Duration::from_micros(5));
     // Set back to reading ROM
     gpio.write(READ_PIN_ID, Level::Low);
     gpio.write(WRITE_PIN_ID, Level::High);
-    for pin in data_pins.iter() {
-        gpio.write(pin.clone(), Level::Low);
-        gpio.set_mode(pin.clone(), Mode::Input);
+    for pin in DATA_PINS.iter() {
+        gpio.write(*pin, Level::Low);
+        gpio.set_mode(*pin, Mode::Input);
     }
 }
 
